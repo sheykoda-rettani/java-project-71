60c483c489399922ee1df12f5606a1830cc78094
Dmitrii Sheiko <sheykoda@gmail.com>
2026-02-25 09:49:11 +0300

    added checkstyle, refactor

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,8 +3,6 @@ package hexlet.code;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -21,20 +19,36 @@ import picocli.CommandLine.Option;
         description = "Compares two configuration files and shows a difference.",
         showDefaultValues = true)
 public final class App implements Callable<Integer> {
-    ObjectMapper objectMapper = new ObjectMapper();
+    /**
+     * Для парсинга JSON файлов.
+     */
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * Help-комманда.
+     */
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     private boolean helpRequested;
 
+    /**
+     * Version-комманда.
+     */
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     private boolean versionInfoRequested;
 
+    /**
+     * Путь к 1-му файлу.
+     */
     @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filePath1;
 
+    /**
+     * Путь ко 2-му файлу.
+     */
     @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to first file")
     private String filePath2;
 
-    public static void main(String... args) {
+    public static void main(final String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
@@ -43,38 +57,41 @@ public final class App implements Callable<Integer> {
     public Integer call() throws Exception {
         Map<String, Object> beforeMap = parseFile(filePath1);
         Map<String, Object> afterMap = parseFile(filePath2);
-        List<String> resultLines = compareMaps(beforeMap, afterMap);
-        System.out.println("{\n" +
-                String.join("\n", resultLines) +
-                "\n}");
+        System.out.println(compareMaps(beforeMap, afterMap));
         return CommandLine.ExitCode.OK;
     }
 
-    private static List<String> compareMaps(Map<String, Object> beforeMap, Map<String, Object> afterMap) {
+    private static String compareMaps(final Map<String, Object> beforeMap, final Map<String, Object> afterMap) {
+        final String added = "  + ";
+        final String removed = "  - ";
+        final String separator = ": ";
+        final String unChanged = "   ";
         Set<String> allKeys = new TreeSet<>(beforeMap.keySet());
         allKeys.addAll(afterMap.keySet());
 
-        List<String> resultLines = new ArrayList<>();
-
+        StringBuilder resultBuilder = new StringBuilder("{").append(System.lineSeparator());
         for (String key : allKeys) {
             if (!beforeMap.containsKey(key)) {
-                resultLines.add("+ " + key + ": " + afterMap.get(key));
+                resultBuilder.append(added).append(key).append(separator).append(afterMap.get(key));
             } else if (!afterMap.containsKey(key)) {
-                resultLines.add("- " + key + ": " + beforeMap.get(key));
+                resultBuilder.append(removed).append(key).append(separator).append(beforeMap.get(key));
             } else if (!Objects.equals(beforeMap.get(key), afterMap.get(key))) {
-                resultLines.add("- " + key + ": " + beforeMap.get(key));
-                resultLines.add("+ " + key + ": " + afterMap.get(key));
+                resultBuilder.append(removed).append(key).append(separator).append(beforeMap.get(key));
+                resultBuilder.append(System.lineSeparator());
+                resultBuilder.append(added).append(key).append(separator).append(afterMap.get(key));
             } else {
-                resultLines.add("  " + key + ": " + beforeMap.get(key));
+                resultBuilder.append(unChanged).append(key).append(separator).append(beforeMap.get(key));
             }
+            resultBuilder.append(System.lineSeparator());
         }
-        return resultLines;
+        resultBuilder.append("}");
+        return resultBuilder.toString();
     }
 
-    private Map<String, Object> parseFile(String filePath) throws IOException {
+    private Map<String, Object> parseFile(final String filePath) throws IOException {
         try {
-            byte[] contentBytes = Files.readAllBytes(Path.of(filePath)); // Читаем весь файл в байтовый массив
-            return objectMapper.readValue(contentBytes, new TypeReference<>() {}); // Десериализируем контент в map
+            byte[] contentBytes = Files.readAllBytes(Path.of(filePath));
+            return objectMapper.readValue(contentBytes, new TypeReference<>() { });
         } catch (IOException e) {
             throw new IOException("Error while parsing file: " + filePath, e);
         }
