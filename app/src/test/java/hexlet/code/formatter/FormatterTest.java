04b7221a6e23173990807edf9b2a3a80806dbb6a
Dmitrii Sheiko <sheykoda@gmail.com>
2026-03-12 07:33:58 +0300

    fixed tests

--- app/src/test/java/hexlet/code/formatter/FormatterTest.java
+++ app/src/test/java/hexlet/code/formatter/FormatterTest.java
@@ -4,18 +4,16 @@ import hexlet.code.Differ;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.api.Disabled;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled("temporarily disable test while fixing data")
 @SuppressWarnings({"checkstyle:MagicNumber", "checkstyle:MultipleStringLiterals"})
 public final class FormatterTest {
     /**
@@ -24,48 +22,49 @@ public final class FormatterTest {
      */
     private static final String RESOURCES_DIR = "src/test/resources";
 
-    void testDefault() {}
+    /**
+     * Разрешения с которыми умеет работать программа
+     */
+    private static final List<String> EXTENSIONS = List.of("json", "yml");
 
-    void testOutput(final String fileNumber, final String fileExt) { }
+    private static final List<String> FORMATS = List.of("stylish", "plain", "json");
 
-    @ParameterizedTest(name = "{index}: Test with format={0}, file number={1}")
-    @MethodSource("provideArguments2")
-    void testOutput(final String format, final int fileNumber) throws IOException {
-        String beforePath = "%s/before_%d.json".formatted(RESOURCES_DIR, fileNumber);
-        String afterPath = "%s/after_%d.json".formatted(RESOURCES_DIR, fileNumber);
-        String expectedOutputPath = "%s/expected_output_%s_%d.txt".formatted(RESOURCES_DIR, format, fileNumber);
+    /**
+     * Проверка поведения по умолчанию, без аргументов. Ожидается, что вывод такой же как у stylish
+     */
+    @ParameterizedTest(name = "{index}: Testing default with extension={0}")
+    @MethodSource("argumentsDefault")
+    void testDefault(final String extension) throws IOException {
+        String beforePath = "%s/before.%s".formatted(RESOURCES_DIR, extension);
+        String afterPath = "%s/after.%s".formatted(RESOURCES_DIR, extension);
+        String expectedOutputPath = "%s/expected_output_stylish.txt".formatted(RESOURCES_DIR);
+        String expectedOutput = Files.readString(Path.of(expectedOutputPath));
+        String actualOutput = Differ.generate(beforePath, afterPath);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    /**
+     * Проверка обычных запусков Differ
+     */
+    @ParameterizedTest(name = "{index}: Testing parametrized with extension={0} an format={1}")
+    @MethodSource("argumentsParametrized")
+    void testParametrized(final String extension, final String format) throws IOException {
+        String beforePath = "%s/before.%s".formatted(RESOURCES_DIR, extension);
+        String afterPath = "%s/after.%s".formatted(RESOURCES_DIR, extension);
+        String expectedOutputPath = "%s/expected_output_%s.txt".formatted(RESOURCES_DIR, format);
         String expectedOutput = Files.readString(Path.of(expectedOutputPath));
         String actualOutput = Differ.generate(beforePath, afterPath, format);
         assertEquals(expectedOutput, actualOutput);
     }
 
-    private static List<Arguments> provideArguments2() {
-        final int lowerBound = 1;
-        final int upperBound = 3;
-        int[] fileNumbers = IntStream.rangeClosed(lowerBound, upperBound).toArray();
-        List<String> allowedFormats = List.of("stylish", "plain", "json");
-        List<Arguments> result = new ArrayList<>();
-        for (String format: allowedFormats) {
-            for (int fileNumber: fileNumbers) {
-                result.add(Arguments.of(format, fileNumber));
-            }
-        }
-
-        return result;
+    private static Stream<Arguments> argumentsDefault() {
+        return EXTENSIONS.stream().map(Arguments::of);
     }
 
-    /**
-     * Генерирует тестовые данные
-     * @return Набор тестовых данных (2 типа файлов и 4 вида "вывода", один из которых пустой)
-     */
-    private static List<Arguments> provideArguments() {
-        final List<String> extensions = List.of("json", "yml");
-        final List<String> allowedFormats = new ArrayList<>();
-        allowedFormats.add(null);
-        allowedFormats.addAll(List.of("stylish", "plain", "json"));
+    private static List<Arguments> argumentsParametrized() {
         List<Arguments> result = new ArrayList<>();
-        for (String extension: extensions) {
-            for (String format: allowedFormats) {
+        for(String extension : EXTENSIONS) {
+            for(String format : FORMATS) {
                 result.add(Arguments.of(extension, format));
             }
         }
